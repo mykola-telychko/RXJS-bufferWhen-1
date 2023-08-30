b6b407054842023a0e811c26f5c91e553315bc10
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 00:50:49 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
 import { interval } from 'rxjs';
 import { bufferWhen } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/bufferwhen
+// Example 1: Emit buffer based on interval
+
 //emit value every 1 second
 const oneSecondInterval = interval(1000);
 //return an observable that emits value every 5 seconds
 const fiveSecondInterval = () => interval(5000);
 //every five seconds, emit buffered values
-const bufferWhenExample = oneSecondInterval.pipe(bufferWhen(fiveSecondInterval));
-//log values
-//ex. output: [0,1,2,3]...[4,5,6,7,8]
-const subscribe = bufferWhenExample.subscribe(val =>
+const bufferWhenExample = oneSecondInterval.pipe(
+  bufferWhen(fiveSecondInterval)
+);
+// output: [0,1,2,3]...[4,5,6,7,8]
+const subscribe = bufferWhenExample.subscribe((val) =>
   console.log('Emitted Buffer: ', val)
 );
